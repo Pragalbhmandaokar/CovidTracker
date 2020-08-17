b2064e6b6dc92d4babf197cb6d77f18f02164006
Amey Khaire <ameybro11@gmail.com>
2020-08-17 16:43:47 +0530

    New API

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,23 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.covid19india.org/v4/min/timeseries-MH.min.json";
+const BASE_URL = "https://mh34-api.vercel.app/temp/data.json";
 
 const fetchData = async () => {
-	let formatedData = null;
-	const newData = [];
 	try {
 		const { data } = await axios.get(BASE_URL);
-		formatedData = data["MH"]["districts"]["Chandrapur"]["dates"];
-		Object.keys(formatedData).forEach(item => {
-			newData.push({
-				date: item,
-				confirmed: formatedData[item].total.confirmed,
-				recovered: formatedData[item].total.recovered,
-				deceased: formatedData[item].total?.deceased,
-				tested: formatedData[item].total?.tested,
-			});
-		});
-		return newData;
+		return data;
 	} catch (error) {
 		return error;
 	}
